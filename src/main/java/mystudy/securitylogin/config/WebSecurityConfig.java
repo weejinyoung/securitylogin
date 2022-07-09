e89d2a739cd0039a0e470083a7e91e6c29658298
Jn W <a8118199@gmail.com>
2022-07-09 16:29:02 +0900

    springsecurity - oauth2 연동설정

--- src/main/java/mystudy/securitylogin/config/WebSecurityConfig.java
+++ src/main/java/mystudy/securitylogin/config/WebSecurityConfig.java
@@ -4,20 +4,27 @@ import lombok.RequiredArgsConstructor;
 import mystudy.securitylogin.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//@Configuration
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
 public class WebSecurityConfig {
 
-//    private final MemberService memberService;
+    private final MemberService memberService;
 
-//    @Bean
-//    PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
 }
